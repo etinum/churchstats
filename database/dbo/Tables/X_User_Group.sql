29a1f9c3257db9a73eb4124b4683ca7ad9b44e14
ET Mbpro <eric.n.tran@gmail.com>
2016-05-21 16:32:51 -0700

    Database schema change

--- database/dbo/Tables/X_User_Group.sql
+++ database/dbo/Tables/X_User_Group.sql
@@ -1,5 +1,9 @@
 ﻿CREATE TABLE [dbo].[X_User_Group] (
     [UserId]  INT NOT NULL,
-    [GroupId] INT NOT NULL
+    [GroupId] INT NOT NULL,
+    CONSTRAINT [FK_X_User_Group_Group] FOREIGN KEY ([GroupId]) REFERENCES [dbo].[Group] ([Id]),
+    CONSTRAINT [FK_X_User_Group_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([Id])
 );
+
+
 
