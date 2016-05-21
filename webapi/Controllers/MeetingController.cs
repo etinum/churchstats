e012bf10b3f979d41d4b9c506668041bdf2df66b
ET Mbpro <eric.n.tran@gmail.com>
2016-05-21 16:59:12 -0700

    Added User and Meeting controllers

---
+++ webapi/Controllers/MeetingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using data;
+
+namespace webapi.Controllers
+{
+    public class MeetingController : ApiController
+    {
+        public IEnumerable<Meeting> Get()
+        {
+            return new Meeting[] {new Meeting() {MeetingName = "TestMeeting", Description = "Test Description"}};
+        }
+    }
+}
