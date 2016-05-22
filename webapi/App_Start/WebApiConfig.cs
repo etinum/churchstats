f8d66ae4a67b9aa765866c194bb3e804fd7f4bbb
ET Mbpro <eric.n.tran@gmail.com>
2016-05-21 17:45:10 -0700

    Made JSON the default format

--- webapi/App_Start/WebApiConfig.cs
+++ webapi/App_Start/WebApiConfig.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace webapi
@@ -17,8 +20,16 @@ namespace webapi
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{action}/{id}",
-                defaults: new {id = RouteParameter.Optional}
-                );
+                defaults: new { id = RouteParameter.Optional}
+            );
+
+            // Return camel casing... 
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            var jsonFormatter = formatters.JsonFormatter;
+            var settings = jsonFormatter.SerializerSettings;
+            settings.Formatting = Formatting.Indented;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
         }
     }
