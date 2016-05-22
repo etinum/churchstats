8108dc089f83c8c8fd6aa5093cb993e2c9e5a022
ET Mbpro <eric.n.tran@gmail.com>
2016-05-21 18:17:11 -0700

    Refactored to use better patterns

---
+++ data/UserLogic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data
+{
+    public class UserLogic
+    {
+        private chruchstatsEntities _ctx;
+
+        public UserLogic()
+        {
+            _ctx = new chruchstatsEntities();
+        }
+
+        public User GetUserById(int id)
+        {
+            return _ctx.Users.FirstOrDefault(u => u.Id == id);
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            return _ctx.Users.ToList();
+        }
+
+        public bool Update(User user)
+        {
+            if (user == null || user.Id <= 0) return false;
+
+            var olduser = _ctx.Users.FirstOrDefault(u => u.Id == user.Id);
+
+            if (olduser == null) return false;
+
+            _ctx.Users.AddOrUpdate(user);
+            _ctx.SaveChanges();
+
+            return true;
+        }
+    }
+}
