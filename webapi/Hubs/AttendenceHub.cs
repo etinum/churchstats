1ff47a0a5c718d354ae4d9829eb4fef03ddf8fec
Daniel deLamare <daniel.delamare@live.com>
2016-05-24 00:27:38 -0700

    added signalr libraries

---
+++ webapi/Hubs/AttendenceHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+namespace webapi
+{
+    public class AttendenceHub : Hub
+    {
+        public void SyncRadioButtons(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.syncRadioButtons(name, message);
+        }
+    }
+}
