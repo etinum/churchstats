c6d93403ba7a4f3a218afd8a0d717cdb280db4a7
Chris <Chris@FFPC>
2016-06-19 15:52:23 -0700

    Add attendance controller and methods

---
+++ webapi/Controllers/AttendanceController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using webapi.Mappers;
+using webapi.ViewModels;
+
+namespace webapi.Controllers
+{
+    public class AttendanceController : ApiController
+    {
+        IMapper mapper;
+        AttendanceLogic attendanceLogic;
+        public AttendanceController()
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.AddProfile<ModelMapper>();
+            });
+            mapper = config.CreateMapper();
+            attendanceLogic = new AttendanceLogic();
+        }
+        public IEnumerable<AttendanceModel> GetMeetingUpdate(int meetingId, DateTime updatesSince)
+        {
+            return mapper.Map<IEnumerable<Attendance>,IEnumerable<AttendanceModel>>( attendanceLogic.GetMeetingUpdate(meetingId, updatesSince));
+        }
+
+        public bool SaveAttendance(int userId, int meetingId, bool isAttend, int recorderId)
+        {
+            return attendanceLogic.SaveAttendance(userId, meetingId, isAttend, recorderId);
+        }
+    }
+}
